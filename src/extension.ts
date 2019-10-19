560c083018916dfb47b5f88cb276afc190881a8b
Yudu Ban <banyudu@gmail.com>
2019-10-19 20:30:11 +0800

    chore: use eslint instead of tslint

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,16 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {}
+export function activate (context: vscode.ExtensionContext): void {
+  // nothing
+}
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate (): void {
+  // nothing
+}
 
-console.log('hello world!');
+console.log('hello world!')
